c2990211274b30a3c5bbe533fd1930b666666a03
andresg19 <andygermain17@gmail.com>
2022-11-04 07:17:30 -0300

    empiezo user profile

--- client/src/Components/UserProfile/Profile.jsx
+++ client/src/Components/UserProfile/Profile.jsx
@@ -9,8 +9,13 @@ const Profile = () => {
       console.log(user)
 
     return ( 
-        <div>
-
+        <div className='containerProfile'>
+             <section className='profileUser'>
+              <img src="https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg" 
+              alt="not found" width={50}  />
+              <h1>{user.fullname}</h1>
+              <h1>{user.email}</h1>
+             </section>
         </div>
      );
 }
