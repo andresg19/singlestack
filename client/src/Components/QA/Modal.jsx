af48be8245e47a2a776dbbc892509429a7b9f8c7
ValentinoMartz <valentinomartinez55@gmail.com>
2022-11-10 17:33:05 -0300

    modified:   api/src/routes/posts.js 	modified:   client/src/Components/QA/Modal.jsx

--- client/src/Components/QA/Modal.jsx
+++ client/src/Components/QA/Modal.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { postPost } from "../../Redux/Actions/Actions";
 
-const Modal = ({  }) => {
+const Modal = ({}) => {
   const dispatch = useDispatch();
   const [input, setInput] = useState({
     title: "",
@@ -10,18 +10,16 @@ const Modal = ({  }) => {
     author: JSON.parse(localStorage.getItem("currentUser")).fullname,
     etiquetas: "",
   });
-  const [img, setImg] =  useState("");
-  
-  console.log(input)
+  const [img, setImg] = useState("");
+
+  console.log(input);
   //  let fullname = currentUser.fullname;
   //  console.log('soyfullname', fullname)
-  // setInput({ ...input, author: fullname }); 
+  // setInput({ ...input, author: fullname });
   const handleChange = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
 
-  
-  
   // const handleImage = (e) => {
   //   e.preventDefault();
   //   const reader = new FileReader();
@@ -32,7 +30,6 @@ const Modal = ({  }) => {
   //     setImg(readerEvent.target.result);
   //   };
   // }
-   
 
   const handlePost = (e) => {
     e.preventDefault();
@@ -43,7 +40,7 @@ const Modal = ({  }) => {
   };
 
   useEffect(() => {
-      // fullname = localStorage.getItem("fullname"); 
+    // fullname = localStorage.getItem("fullname");
   }, []);
 
   return (
@@ -64,11 +61,11 @@ const Modal = ({  }) => {
           onChange={handleChange}
         />
         <input
-        type="text"
-        name="etiquetas"
-        placeholder="#javascript"
-        value={input.etiquetas}
-        onChange={handleChange}
+          type="text"
+          name="etiquetas"
+          placeholder="#javascript"
+          value={input.etiquetas}
+          onChange={handleChange}
         />
         {/* <input 
         type="file"
