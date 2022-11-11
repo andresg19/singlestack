1ff39777cb0b88f74628e584a2c00c6350123dc3
ValentinoMartz <valentinomartinez55@gmail.com>
2022-11-11 17:21:47 -0300

    borrare dos  /* comments */

--- client/src/Components/QA/InputComment.jsx
+++ client/src/Components/QA/InputComment.jsx
@@ -12,31 +12,8 @@ const InputComment = ({ postId }) => {
   const [input, setInput] = useState("");
   const [img, setImg] = useState("");
 
-//   function base64toBlob(base64Data, contentType) {
-//     contentType = contentType || '';
-//     var sliceSize = 1024;
-//     var byteCharacters = atob(base64Data);
-//     var bytesLength = byteCharacters.length;
-//     var slicesCount = Math.ceil(bytesLength / sliceSize);
-//     var byteArrays = new Array(slicesCount);
-
-//     for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
-//         var begin = sliceIndex * sliceSize;
-//         var end = Math.min(begin + sliceSize, bytesLength);
-
-//         var bytes = new Array(end - begin);
-//         for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
-//             bytes[i] = byteCharacters[offset].charCodeAt(0);
-//         }
-//         byteArrays[sliceIndex] = new Uint8Array(bytes);
-//     }
-//     return new Blob(byteArrays, { type: contentType });
-// }
-
-
   const handleImage = (e) => {
     e.preventDefault();
-    //base64
     const reader = new FileReader();
     if (e.target.files[0]) {
       reader.readAsDataURL(e.target.files[0]);
@@ -71,12 +48,7 @@ const InputComment = ({ postId }) => {
         placeholder="Escribe tu respuesta, cambia el mundo 😏"
         onChange={(e) => setInput(e.target.value)}
       ></textarea>
-      <input
-        type="file"
-        /*  accept=".jpg,.jpeg,.png,.webp,.doc,.blob,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" */
-        onChange={handleImage}
-        // value={img}
-      />
+      <input type="file" onChange={handleImage} />
       {img ? <img src={img} alt="" width={25} /> : <p>No hay imagen</p>}
       <button onClick={handleSubmit}>Responder</button>
     </div>
