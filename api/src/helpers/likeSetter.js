6934d704df8b7b13026d184583af1603885750a8
ValentinoMartz <valentinomartinez55@gmail.com>
2022-11-18 17:28:56 -0300

    all good from andres branch

--- api/src/helpers/likeSetter.js
+++ api/src/helpers/likeSetter.js
@@ -1,28 +1,31 @@
- const likeSetter = async (model, switcher, userId, commentId) => {
+const likeSetter = async (model, switcher, userId, commentId) => {
+  try {
+    let like = await model.findAll({ where: { commentId } });
 
-    try {
-      let like = await model.findAll({ where: { commentId } });
-  
-      switch(switcher) {
-        case 'up':
-          let addOneLike = await model.increment('likes', { by: 1, where: { commentId }});
-          return addOneLike;
-  
-        case "down":
-          let disLike = await model.decrement('likes', { by: 1, where: { commentId }});
-          return disLike;
-          
-        default :
-         let newLike = await model.create({ likes: 1, commentId, userId, });
-         return newLike;
-        }
-        
-      } catch (error) {
-        next(error);
-      }
-      
-}
+    switch (switcher) {
+      case "up":
+        let addOneLike = await model.increment("likes", {
+          by: 1,
+          where: { commentId },
+        });
+        return addOneLike;
+
+      case "down":
+        let disLike = await model.decrement("likes", {
+          by: 1,
+          where: { commentId },
+        });
+        return disLike;
+
+      default:
+        let newLike = await model.create({ likes: 1, commentId, userId });
+        return newLike;
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   likeSetter,
-}
+};
