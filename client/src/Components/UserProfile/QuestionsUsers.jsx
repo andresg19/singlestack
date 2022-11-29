e8a72ed604a977595d1ff3362beb875d1cb891c8
andresg19 <andygermain17@gmail.com>
2022-11-29 16:45:00 -0300

    questionsusers detail avance

--- client/src/Components/UserProfile/QuestionsUsers.jsx
+++ client/src/Components/UserProfile/QuestionsUsers.jsx
@@ -1,64 +1,65 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getPosts } from '../../Redux/Actions/Actions';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "../../Redux/Actions/Actions";
 
 const QuestionsUsers = () => {
-   const posts = useSelector((state) => state.posts)
-   const dispatch = useDispatch();
-   const currentUser = JSON.parse(localStorage.getItem("currentUser")).fullname
-   console.log('soyCurrentUser', currentUser)
-   console.log('soy posts', posts)
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+  const currentUser = JSON.parse(localStorage.getItem("currentUser")).fullname;
+  console.log("soyCurrentUser", currentUser);
+  console.log("soy posts", posts);
 
-   const resultQuestionsUser = []
-   console.log(resultQuestionsUser)
-   let postsCurrentUser = posts.forEach((post) => {
-    console.log(post.author)
+  const resultQuestionsUser = [];
+  console.log(resultQuestionsUser);
+  let postsCurrentUser = posts.forEach((post) => {
+    console.log(post.author);
     if (post.author === currentUser) {
-        resultQuestionsUser.push(post)
+      resultQuestionsUser.push(post);
     }
-   })
+  });
 
-   useEffect(() => {
+  useEffect(() => {
     dispatch(getPosts());
-   }, [])
+  }, []);
 
+  return (
+    <div className="">
+      {resultQuestionsUser.map((q) => {
+        return (
+          <div className="bg-[#163250] m-2 w-[60%] text-white rounded-[2%]">
+            <div className="text-m text-center">
+              <h1
+              className="mt-3"
+              >
+                {q.title}
+                </h1>
+              <p
+              className="mt-3"
+              >
+                {q.content}
+                </p>
+            </div>
 
+            <div>
+              {q.img
+                ? q.img.map((i) => {
+                    return <img src={i} alt="not found" 
+                    className="w-[15%] ml-auto mr-auto mt-[2%]" 
+                    />;
+                  })
+                : null}
+            </div>
 
-
-   return (
-    <div className=''>
-        {
-            resultQuestionsUser.map((q) => {
-                return(
-                    <div>
-                    <div>
-                        <h1>{q.title}</h1>
-                        <p>{q.content}</p>
-                    </div>
-                    <div>
-                        {q.img ? q.img.map((i) => {
-                            return (
-                                <img src={i} alt="not found" className='w-[15%]' />
-                            )
-                        }): null }
-                    </div>
-                    <div>
-
-                        <p>{q.author}</p>
-                        <p>{q.date}</p>
-
-                    </div>
-                    </div>
-
-                )
-            })
-        }
+            <div className="grid mt-[5%] ml-auto w-[25%]">
+              <p>{q.author}</p>
+              <p>{q.date}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
-   )
+  );
+};
 
-   
-
-}
- 
 export default QuestionsUsers;
