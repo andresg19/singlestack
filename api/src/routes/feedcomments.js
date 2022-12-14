d8b6cacad8546046a18f2944e74652b5b6f01fae
ValentinoMartz <valentinomartinez55@gmail.com>
2022-12-14 18:43:52 -0300

    rutas de feedcomments hechas

--- api/src/routes/feedcomments.js
+++ api/src/routes/feedcomments.js
@@ -2,8 +2,25 @@ const { Router } = require("express");
 
 const router = Router();
 
-const {Feedcomments } = require("../db.js");
+const { Feedcomments } = require("../db.js");
 
+router.get("/", async (req, res) => {
+  try {
+    let comments = await Feedcomments.findAll();
+    res.status(200).json(comments);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+router.post("/", async (req, res) => {
+  let { author, feedPostId, content } = req.body;
+  try {
+    let comment = await Feedcomments.create({ author, feedPostId, content });
+    res.status(200).json(comment);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
