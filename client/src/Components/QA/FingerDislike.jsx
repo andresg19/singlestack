f19bd896da02683d58ab935a99307df0354f9008
andresg19 <andygermain17@gmail.com>
2023-02-24 10:08:26 -0300

    likes dislikes posts

--- client/src/Components/QA/FingerDislike.jsx
+++ client/src/Components/QA/FingerDislike.jsx
@@ -13,7 +13,7 @@ const FingerDislike = ({ dislikes, comment, userId }) => {
       version="1.0"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512.000000 512.000000"
-      className="w-9"
+      className="w-10"
     >
       <g
         transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
--- client/src/Components/QA/FingerLike.jsx
+++ client/src/Components/QA/FingerLike.jsx
@@ -13,7 +13,7 @@ const FingerLike = ({ likes, comment, userId }) => {
       version="1.0"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512.000000 512.000000"
-      className="w-10 "
+      className="w-12 "
     >
       <g
         transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
