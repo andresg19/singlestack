961aa0b94f63b34365effe566db02430761ad878
andresg19 <andygermain17@gmail.com>
2023-04-07 06:55:42 -0300

    to main

--- api/index.js
+++ api/index.js
@@ -20,18 +20,12 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
+require('dotenv').config();
+
 // Syncing all the models at once.
-// atento aca
-/* conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
-  });
-}); */
-conn
-  .sync({ force: false })
-  .then(() => {
-    server.listen(3001, () => {
-      console.log("%s listening at 3001"); // eslint-disable-line no-console
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
     });
   })
   .catch((err) => console.log("TODO MAL", err));
